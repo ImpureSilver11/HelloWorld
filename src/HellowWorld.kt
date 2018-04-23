6684d7a655e20205d429b0b203fbd257f9a178ac
MasahiroTakahashi <take1931032@gmail.com>
2018-04-23 19:54:45 +0900

    masa
    3章4節まで

--- src/HellowWorld.kt
+++ src/HellowWorld.kt
@@ -1,20 +1,24 @@
 
 package sample
 
-class Rational(val numberator: Int, val denominator: Int){
+class Rational(val n: Int, val d: Int){
 init{
-    require(denominator !=0 ,{"denominator must not be null"})
+    require(d!=0 ,{"denominator must not be null"})
 }
     override fun toString(): String = "${numberator}/${denominator}"
+private val g = gcd(Math.abs(n) ,Math.abs(d))
+    val numberator: Int = n / g
+    val denominator: Int = d / g
+tailrec private fun gcd(a: Int, b:Int): Int =
+        if (b == 0 )a
+    else gcd(b,a%b)
 }
 
 
 fun main(args: Array<String>){
-val harl = Rational(2, 0)
-
-
-    println(harl)
-
-
-    println("hello world")
+val harl = Rational(17, 17)
+    println(harl) 
+    println(Rational(55,100))
+    println(Rational(1234,5678))
+//    println("hello world")
 }
